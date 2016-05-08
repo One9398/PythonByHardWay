8cfac403aca643589b90f1e8238f7b30786b1570
Simon <oneSimon9398@gmail.com>
2016-05-08 11:53:07 +0800

    use basic string format to log message

---
+++ formateString.py
@@ -0,0 +1,23 @@
+
+
+#! /usr/bin/env python
+# -*- coding: gbk -*-
+
+name = "Simon"
+age = 23
+weight = 55.0
+height = "173 cm"
+hobby = "reading"
+
+print "Let's talk about %s." % name
+print "He's %s tall" % height
+print "He's %f heavy" % weight
+print "He's %d years old" % age
+print "He likes %s" % hobby
+
+"""
+
+Document aboout String Format:
+https://docs.python.org/2/library/string.html?highlight=string%20format#string.Formatter.format
+
+"""
