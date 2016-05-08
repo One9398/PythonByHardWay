d45c36c820f8767466e3a22da5a6d781c1df05b6
Simon <oneSimon9398@gmail.com>
2016-05-08 10:34:34 +0800

    - use 'print' to log message. - use '#' to comment something.

---
+++ print.py
@@ -0,0 +1,11 @@
+
+
+print "Hello Word!"
+print "Hello Again!"
+print "Hello Programmer!"
+print "I like typing this."
+print "Yay! I'm printing."
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+# print "it is a comment, and doesn't print"
+
