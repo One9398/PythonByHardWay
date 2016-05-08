fcecfc38010031529ff2df29e433dadb80c36dab
Simon <oneSimon9398@gmail.com>
2016-05-08 10:50:08 +0800

    use basic opear to simple calculate.

---
+++ calculate.py
@@ -0,0 +1,11 @@
+
+
+
+print "sum ", 10 + 11, " = 10 + 11"
+print "reduce", 10 - 11, "= 10 - 11"
+print "multi", 10 * 11, "= 10 * 11"
+print "division", 25 / 4, "= 25 / 4"
+print "division-float", 25.0 /4, "= 25 / 4.0" # the same result as 25.0 / 4
+print "mod", 25 % 4, "= 25 % 4"
+
+
